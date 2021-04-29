f7750ae8100ed23104fa3bbc7a7e8987c274d81c
bekir <pmyoviki@gmail.com>
2021-04-29 16:50:39 +0300

    Consumer-Producer class added

---
+++ nw.py
@@ -0,0 +1,19 @@
+
+import re
+import proxy , time
+import webdriver
+import queue
+import threading
+import webdriver as webdriver_class
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+list = ["data","data2","data3"]
+
+# with open('/home/bekir/Desktop/json.txt','a') as file:
+#     file.write(list.)
+
+
+
